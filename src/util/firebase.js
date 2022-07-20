43e8dcf3bb3fc42a7547d67647ba48ece729071d
ivndot <ivan_alamos@outlook.com>
2022-07-19 21:09:22 -0500

    Changes: --> Add new gitignore file --> Change permissions file to be an js object --> Add dotenv file

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,6 +1,6 @@
 //firestore
 const admin = require("firebase-admin");
-const serviceAccount = require("../../permissions.json");
+const serviceAccount = require("../../permissions");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://api-stpharm.firebaseio.com",
